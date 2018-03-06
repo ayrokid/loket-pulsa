f6dee0a8275beef805921e059197e19d925380bc
ayrokid <ayrokid@gmail.com>
2018-03-06 07:55:31 +0700

    pwa

---
+++ layouts/blog.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <!-- Navigation -->
+    <Navigation/>
+    <!-- Content -->
+    <div class="container content">
+      <div class="row">
+        <div class="col-md-9 col-lg-9">
+          <!-- Content -->
+          <Banner />
+          <nuxt/>
+        </div>
+        <div class="col-md-3 col-lg-3">
+          <!-- Sidebar -->
+          <div class="sidebar">
+            <h4>Title of sidebar</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, porro repudiandae. Tempora, ab nostrum dolorum temporibus maiores architecto, distinctio dolorem id modi, neque atque ipsam dicta sit eum non. Sed.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Footer -->
+    <div class="footer fixed-bottom">
+      <div class="d-inline p-2 bg-primary text-white">d-inline</div>
+<div class="d-inline p-2 bg-dark text-white">d-inline</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navigation from '~/components/Navigation.vue'
+import Banner from '~/components/Banner.vue'
+export default {
+  components: {
+    Navigation,
+    Banner
+  }
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,87 @@
+module.exports = {
+  /*
+  ** Build configuration
+  */
+  build: {
+    extend (config, ctx) {
+      if (ctx.isDev && ctx.client) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+
+        const vueLoader = config.module.rules.find(
+          ({ loader }) => loader === 'vue-loader')
+        const { options: { loaders } } = vueLoader || { options: {} }
+        if (loaders) {
+          for (const loader of Object.values(loaders)) {
+            changeLoaderOptions(Array.isArray(loader) ? loader : [loader])
+          }
+        }
+        config.module.rules.forEach(rule => changeLoaderOptions(rule.use))
+        // console.log(util.inspect(config.module.rules, { depth: 6 }))
+      }
+    }
+  },
+  /*
+  ** Headers
+  ** Common headers are already provided by @nuxtjs/pwa preset
+  */
+  head: {
+    title: 'example',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
+
+      // example vue-meta
+      { hid: 'title', name: 'title', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/icon.png' },
+      { rel: 'stylesheet', type: 'text/css', href: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' },
+      { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.0.7/css/swiper.css' }
+    ]
+  },
+
+  css: [
+    { src: '~/assets/scss/navigation.scss', lang: 'scss' }
+  ],
+  /*
+  ** Custom Plugin
+   */
+  plugins: [
+    { src: '~plugins/swiper.js', ssr: false }
+  ],
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Customize app manifest
+  */
+  manifest: {
+    theme_color: '#3B8070'
+  },
+  /*
+  ** Modules
+  */
+  modules: [
+    '@nuxtjs/pwa'
+  ]
+}
+
+function changeLoaderOptions (loaders) {
+  if (loaders) {
+    for (const loader of loaders) {
+      if (loader.loader === 'sass-loader') {
+        Object.assign(loader.options, {
+          includePaths: ['./assets']
+          // data: '@import "_imports";'
+        })
+      }
+    }
+  }
+}
