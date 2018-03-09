48e1ccce7bb7fb6b59c5edca9cbea9ca221f13d0
ayrokid <ayrokid@gmail.com>
2018-03-09 16:11:19 +0700

    background

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -6,13 +6,13 @@
       </h1>
       <form @submit.prevent="login">
         <div class="form-group">
-            <input type="email" class="form-control" v-model="email" aria-describedby="emailHelp" placeholder="Enter email">
-            <div class="invalid-feedback">
-                Please choose a username.
-            </div>
+          <input v-model="email" type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email">
+          <div class="invalid-feedback">
+            Please choose a username.
+          </div>
         </div>
         <div class="form-group">
-            <input type="password" class="form-control" v-model="password" placeholder="Password">
+          <input v-model="password" type="password" class="form-control" placeholder="Password">
         </div>
       
         <button type="submit" class="btn btn-outline-primary btn-block" >Submit</button>
@@ -29,60 +29,59 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        email: '',
-        password: '',
-        ip: 0,
-        loading: false
-      }
-    },
-    mounted () {
-
-    },
-    methods: {
-      async login () {
-        // const { email, password } = this
-        // const data = await this.$axios.$post('https://api-sm.s45.in/me/login', {email, password})
-        // this.ip = data
-        // console.log(this.ip)
-        this.$toast.success('Logging in...').goAway(1500)
-      }
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      ip: 0,
+      loading: false
+    }
+  },
+  mounted() {},
+  methods: {
+    async login() {
+      // const { email, password } = this
+      // const data = await this.$axios.$post('https://api-sm.s45.in/me/login', {email, password})
+      // this.ip = data
+      // console.log(this.ip)
+      this.$toast.success("Logging in...").goAway(1500)
     }
   }
+}
 </script>
 
 <style scoped>
-  .container {
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  }
-  .content {
-    width: 100%;
-    margin: 50px;
-  }
-  .title {
-    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-    display: block;
-    font-weight: 300;
-    font-size: 60px;
-    color: #35495e;
-    letter-spacing: 1px;
-  }
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.content {
+  width: 100%;
+  margin: 50px;
+}
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 60px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
 
-  .subtitle {
-    font-weight: 300;
-    font-size: 20px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
+.subtitle {
+  font-weight: 300;
+  font-size: 20px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
 
-  .links {
-    padding-top: 15px;
-  }
+.links {
+  padding-top: 15px;
+}
 </style>
--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -6,10 +6,10 @@
       </h1>
       <form @submit.prevent="login">
         <div class="form-group">
-            <input type="email" class="form-control" v-model="email" aria-describedby="emailHelp" placeholder="Enter email">
-            <div class="invalid-feedback">
-                Please choose a email.
-            </div>
+          <input v-model="email" type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email">
+          <div class="invalid-feedback">
+            Please choose a email.
+          </div>
         </div>
       
         <button type="submit" class="btn btn-outline-primary btn-block" >Daftar</button>
@@ -22,60 +22,59 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        email: '',
-        password: '',
-        ip: 0,
-        loading: false
-      }
-    },
-    mounted () {
-
-    },
-    methods: {
-      async login () {
-        // const { email, password } = this
-        // const data = await this.$axios.$post('https://api-sm.s45.in/me/login', {email, password})
-        // this.ip = data
-        // console.log(this.ip)
-        this.$toast.success('Logging in...').goAway(1500)
-      }
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      ip: 0,
+      loading: false
+    }
+  },
+  mounted() {},
+  methods: {
+    async login() {
+      // const { email, password } = this
+      // const data = await this.$axios.$post('https://api-sm.s45.in/me/login', {email, password})
+      // this.ip = data
+      // console.log(this.ip)
+      this.$toast.success("Logging in...").goAway(1500)
     }
   }
+}
 </script>
 
 <style scoped>
-  .container {
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  }
-  .content {
-    width: 100%;
-    margin: 50px;
-  }
-  .title {
-    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-    display: block;
-    font-weight: 300;
-    font-size: 60px;
-    color: #35495e;
-    letter-spacing: 1px;
-  }
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.content {
+  width: 100%;
+  margin: 50px;
+}
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 60px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
 
-  .subtitle {
-    font-weight: 300;
-    font-size: 20px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
+.subtitle {
+  font-weight: 300;
+  font-size: 20px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
 
-  .links {
-    padding-top: 15px;
-  }
+.links {
+  padding-top: 15px;
+}
 </style>
